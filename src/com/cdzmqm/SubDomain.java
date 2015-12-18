9a2406c420b0f9716a653fa3c049312ab8521b36
Chora <YumsaChora@aliyun.com>
2015-12-18 08:56:14 +0800

    Update

--- src/com/cdzmqm/SubDomain.java
+++ src/com/cdzmqm/SubDomain.java
@@ -1,17 +1,8 @@
 package com.cdzmqm;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Scanner;
-
-
 public class SubDomain {
 	
-	public static void main(String[] args) throws IOException  {
+	public static void main(String[] args) {
 		
 	}
-
 }
